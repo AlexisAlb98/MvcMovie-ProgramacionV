2617ba70c7450443311040dd9f13697b61333ed1
AlexisAlb98 <alexisalbarracin2014@gmail.com>
2024-04-07 18:12:30 -0300

    Semana 3: Se crean los html en la carpeta HelloWorld.

--- Controllers/HelloWorldController.cs
+++ Controllers/HelloWorldController.cs
@@ -1,21 +1,24 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using System.Text.Encodings.Web;
 
+
 namespace MvcMovie.Controllers
 {
     public class HelloWorldController : Controller
     {
-        public string Index()
-       {
-           return "This is my default action...";
-       }
-
-        //
-        // GET: /HelloWorld/Welcome/ Agregar este método:
-
-        public string Welcome(string name, int ID = 1)
+        public IActionResult Index()
         {
-            return HtmlEncoder.Default.Encode($"Hello {name}, ID: {ID}");
+            return View();
+        }
+
+
+        public IActionResult Welcome(string name, int numTimes = 1)
+        {
+            ViewData["Message"] = "Hello " + name;
+            ViewData["NumTimes"] = numTimes;
+
+
+            return View();
         }
     }
 }
